acc2a6dfb6293e12adfe92ff74f063fd982d16f1
Rogerioerio <roger.marcondes19@gmail.com>
2023-07-10 20:17:50 -0300

    Creating Seller Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	
 	public static void main(String[] args) {
 
-		Department d = new Department(1,"Peixe");
+		Department d = new Department(1,"Fish");
+		
+		Seller s = new Seller(1, "Rogerio", "rogerio@gmail.com", new Date(), 3000.00, d);
 		
 		System.out.println(d);
+		System.out.println(s);
 	}
 	
 }
