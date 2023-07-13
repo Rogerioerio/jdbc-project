1883adb81f29ec0529fa76c2061dd43933e13684
Rogerioerio <roger.marcondes19@gmail.com>
2023-07-12 22:08:04 -0300

    findById n update implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -21,11 +21,20 @@ public class Program2 {
 			System.out.println(obj);
 		}
 		
-		System.out.println("---Test 2: department insert");		
-		Department newDep = new Department(null, "Secretary");
-		departmentDao.insert(newDep);
-		System.out.println("Inserted! New id = " + newDep.getId());		
+		//System.out.println("\n---Test 2: department insert");		
+		//Department newDep = new Department(null, "Secretary");
+		//departmentDao.insert(newDep);
+		//System.out.println("Inserted! New id = " + newDep.getId());		
 		
+		System.out.println("\n---Test 3: department findById");	
+		Department dep = departmentDao.findById(3);
+		System.out.println(dep);
+		
+		System.out.println("\n---Test 5: seller update");	
+		dep = departmentDao.findById(6);
+		dep.setName("Hospital");
+		departmentDao.update(dep);
+		System.out.println("Update completed!");
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -12,6 +12,7 @@ import db.DB;
 import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class DepartmentDaoJDBC implements DepartmentDao{
 	
@@ -56,8 +57,22 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public void update(Department obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 		
+		try {
+			st = conn.prepareStatement("UPDATE department SET Name = ? WHERE Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+			
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
@@ -68,7 +83,28 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT * from department WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			if (rs.next()) {
+				Department obj = instantiateDepartment(rs);
+				return obj;
+			}
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 		return null;
 	}
 
